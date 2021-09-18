aeb9a3ad9a32498642dc7bbd2206a98a60b825f6
傅双亮 <shuangliang.fu@baozun.com>
2021-09-18 15:49:08 +0800

    feat(*): 优化代码

--- java-concurrent/src/main/java/com/maeteno/study/java/concurrent/CountDownLatchExample.java
+++ java-concurrent/src/main/java/com/maeteno/study/java/concurrent/CountDownLatchExample.java
@@ -2,29 +2,25 @@ package com.maeteno.study.java.concurrent;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
+ * 该功能和 go 里面的 sync.WaitGroup 类似
+ *
  * @author Alan.Fu
  */
 @Slf4j
 public class CountDownLatchExample {
-
-
     public static void main(String[] args) {
         final ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(
                 0,
                 Integer.MAX_VALUE,
                 60L,
                 TimeUnit.SECONDS,
-                new SynchronousQueue<Runnable>()
+                new SynchronousQueue<>()
         );
 
         CountDownLatch latch = new CountDownLatch(4);
-
         Runnable runnable = () -> {
             try {
                 Thread.sleep(1000);
@@ -36,16 +32,18 @@ public class CountDownLatchExample {
             latch.countDown();
         };
 
-        poolExecutor.execute(runnable);
+        Future<?> submit = poolExecutor.submit(runnable);
+        log.info("submit {}", submit.isDone());
         poolExecutor.execute(runnable);
         poolExecutor.execute(runnable);
 
         try {
             if (latch.await(3, TimeUnit.SECONDS)) {
                 log.info("任务完成");
-            }else{
+            } else {
                 log.info("任务超时");
             }
+            log.info("submit {}", submit.isDone());
             log.info("End...");
         } catch (InterruptedException e) {
             log.info("InterruptedException", e);
