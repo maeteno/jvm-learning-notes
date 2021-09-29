2f0c782e3efc0505f5d8ef4777ca935204aeccb7
傅双亮 <shuangliang.fu@baozun.com>
2021-09-29 11:17:21 +0800

    feat(java): add netty code

---
+++ java-netty/src/main/java/com/maeteno/study/java/netty/client/NettyClient.java
@@ -0,0 +1,10 @@
+package com.maeteno.study.java.netty.client;
+
+/**
+ * @author Alan.Fu
+ */
+public class NettyClient {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ java-netty/src/main/java/com/maeteno/study/java/netty/server/NettyServer.java
@@ -0,0 +1,10 @@
+package com.maeteno.study.java.netty.server;
+
+/**
+ * @author Alan.Fu
+ */
+public class NettyServer {
+    public static void main(String[] args) {
+
+    }
+}
